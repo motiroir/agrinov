306b311c0f1b173b371ccc50993211fce909293d
unknown <costabello.florent@gmail.com>
2024-10-21 15:20:07 +0200

    DashBoard Wip

---
+++ Controllers/DashBoardController.cs
@@ -0,0 +1,22 @@
+﻿using AgriNov.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriNov.Controllers
+{
+    public class DashBoard : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+           UserAccount userAccount;
+            using (IServiceUserAccount serviceUserAccount = new ServiceUserAccount())
+            {
+                userAccount = serviceUserAccount.GetUserAccountByID(HttpContext.User.Identity.Name);
+            }
+
+
+                return View(userAccount);
+        }
+    }
+}
