2beecc83cf8b62cc2e2596b89411483dd6c7b999
unknown <costabello.florent@gmail.com>
2024-10-22 15:25:07 +0200

    ajout22_10

--- Controllers/DashBoardController.cs
+++ Controllers/DashBoardController.cs
@@ -1,4 +1,5 @@
 ﻿using AgriNov.Models;
+using AgriNov.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,14 +10,37 @@ namespace AgriNov.Controllers
         [Authorize]
         public IActionResult Index()
         {
-           UserAccount userAccount;
-            using (IServiceUserAccount serviceUserAccount = new ServiceUserAccount())
+            UserAccountInfoUpdate viewModel = new UserAccountInfoUpdate();
+            using (ServiceUserAccount serviceUserAccount = new ServiceUserAccount())
             {
-                userAccount = serviceUserAccount.GetUserAccountByID(HttpContext.User.Identity.Name);
+                UserAccount currentAccount = serviceUserAccount.GetUserAccountByIDEager(HttpContext.User.Identity.Name);
+
+                viewModel.UserAccountId = currentAccount.Id;
+                viewModel.UserAccountLevel = currentAccount.UserAccountLevel;
+                // Assign data to viewModel depending on the type of account
+                if (currentAccount.UserAccountLevel == UserAccountLevel.USER || currentAccount.UserAccountLevel == UserAccountLevel.VOLUNTEER || currentAccount.UserAccountLevel == UserAccountLevel.ADMIN)
+                {
+                    viewModel.User = currentAccount.User;
+                    viewModel.Address = currentAccount.User.Address;
+                    viewModel.ContactDetails = currentAccount.User.ContactDetails;
+                }
+                if (currentAccount.UserAccountLevel == UserAccountLevel.CORPORATE)
+                {
+                    viewModel.CorporateUser = currentAccount.CorporateUser;
+                    viewModel.Address = currentAccount.CorporateUser.Address;
+                    viewModel.ContactDetails = currentAccount.CorporateUser.ContactDetails;
+                    viewModel.CompanyDetails = currentAccount.CorporateUser.CompanyDetails;
+                }
+                if (currentAccount.UserAccountLevel == UserAccountLevel.SUPPLIER)
+                {
+                    viewModel.Supplier = currentAccount.Supplier;
+                    viewModel.Address = currentAccount.Supplier.Address;
+                    viewModel.ContactDetails = currentAccount.Supplier.ContactDetails;
+                    viewModel.CompanyDetails = currentAccount.Supplier.CompanyDetails;
+                }
             }
-
-
-                return View(userAccount);
+            return View(viewModel);
         }
-    }
+       
+}
 }
