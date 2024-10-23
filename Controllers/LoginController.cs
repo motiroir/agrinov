1b1fa783e215ed18df7873f84114cfd49a90dd8d
QuentC13 <quentin83640@orange.fr>
2024-10-23 10:27:07 +0200

    Update Url Action for Login, UserDashboard with CSS Dashboard

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -78,7 +78,7 @@ namespace AgriNov.Controllers
                                 }
                             }
                         }
-                        return RedirectToAction("Index","MyAccount"); 
+                        return RedirectToAction("Index","DashBoard"); 
                     }
                     else
                     {
