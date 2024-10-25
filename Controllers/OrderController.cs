806871590a084ed6c3c1d0da5faec2adf3c3ab93
Alexia <alexia.roy@hotmail.fr>
2024-10-25 09:56:14 +0200

    Show all orders for admin

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,123 @@
+﻿using AgriNov.Models;
+using AgriNov.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgriNov.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ServiceOrder sO = new ServiceOrder();
+        private List<SelectListItem> GetEnumSelectListString<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T))
+                .Cast<T>()
+                .Select(e => new SelectListItem
+                {
+                    Value = ((int)(object)e).ToString(),
+                    Text = e.GetDisplayName()
+                }).ToList();
+        }
+
+        [HttpGet]
+        public IActionResult ShowAllOrders(OrderViewModel viewModel)
+        {
+            using (ServiceOrder sO = new ServiceOrder())
+            {
+                viewModel.AllOrders = sO.GetAllOrdersWithoutDetails() ?? new List<Order>();
+            }
+
+            viewModel.PaymentOptions = GetEnumSelectListString<PaymentType>() ?? new List<SelectListItem>();
+
+            viewModel.ReceivedOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Payé", Value = "true" },
+                new SelectListItem { Text = "Non payé", Value = "false" }
+            };
+
+            viewModel.WasDeliveredOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Livré", Value = "true" },
+                new SelectListItem { Text = "En cours", Value = "false" }
+            };
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult UpdateOrderValidation(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+
+            var oldOrder = sO.GetOrderById(id);
+
+            if (oldOrder == null)
+            {
+                return NotFound();
+            }
+
+            OrderViewModel viewModel = new OrderViewModel
+            {
+                Order = oldOrder,
+                PaymentOptions = GetEnumSelectListString<PaymentType>(),
+                
+            };
+            viewModel.ReceivedOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Payé", Value = "true" },
+                new SelectListItem { Text = "Non payé", Value = "false" }
+            };
+
+            viewModel.WasDeliveredOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Livré", Value = "true" },
+                new SelectListItem { Text = "En cours", Value = "false" }
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateOrderValidation(int id, OrderViewModel viewModel)
+        {
+            if (viewModel == null || viewModel.Order == null)
+            {
+                return BadRequest("Données de commande invalides.");
+            }
+
+            var orderToUpdate = sO.GetOrderById(id);
+            if (orderToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            orderToUpdate.WasDelivered = viewModel.Order.WasDelivered;
+            if (viewModel.Order.Payment.Received)
+            {
+                orderToUpdate.Payment.Date = DateTime.Now;
+            }
+            orderToUpdate.Payment.PaymentType = viewModel.Order.Payment.PaymentType;
+            orderToUpdate.Payment.Received = viewModel.Order.Payment.Received;
+            orderToUpdate.DateLastModified = DateTime.Now;
+
+            sO.UpdateOrder(orderToUpdate);
+
+            return RedirectToAction("ShowAllOrders", "Order");
+        }
+
+
+        public IActionResult ShowMyOrders(OrderViewModel viewModel)
+        {
+           
+            return View(viewModel);
+        }
+
+        public IActionResult OrderDetails()
+        {
+            return View();
+        }
+    }
+}
--- Models/OrdersModel/ServiceShoppingCart.cs
+++ Models/OrdersModel/ServiceShoppingCart.cs
@@ -185,14 +185,27 @@ namespace AgriNov.Models
             AddProductToShoppingCart(1, 3, 1);
             AddProductToShoppingCart(1, 1, 2);
             AddProductToShoppingCart(2, 1, 2);
+            AddProductToShoppingCart(5, 1, 2);  
             AddProductToShoppingCart(3, 1, 3);
+            AddProductToShoppingCart(6, 1, 3); 
             AddProductToShoppingCart(3, 1, 4);
             AddProductToShoppingCart(3, 1, 12);
+            AddProductToShoppingCart(1, 2, 15);  
+            AddProductToShoppingCart(1, 3, 17);   
+            AddProductToShoppingCart(1, 1, 10);  
+            AddProductToShoppingCart(3, 1, 9);  
+            AddProductToShoppingCart(8, 1, 9);  
             AddBoxContractToShoppingCart(1,1,12);
             AddBoxContractToShoppingCart(6,1,4);
             AddBoxContractToShoppingCart(7,1,4);
             AddBoxContractToShoppingCart(8,1,4);
             AddBoxContractToShoppingCart(9,2,4);
+            AddBoxContractToShoppingCart(1, 1, 15);
+            AddBoxContractToShoppingCart(6, 1, 15);
+            AddBoxContractToShoppingCart(7, 1, 10);
+            AddBoxContractToShoppingCart(8, 1, 9);
+            AddBoxContractToShoppingCart(9, 2, 9);
+
 
         }
 
