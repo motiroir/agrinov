f96933112fc235086372978cb290f1d0a9a576c9
motiroir <contact.tiroirmorgane@gmail.com>
2024-10-26 21:17:57 +0200

    realistic mockup images on products

--- Models/ProductModel/ProductService.cs
+++ Models/ProductModel/ProductService.cs
@@ -76,12 +76,12 @@ namespace AgriNov.Models
                 Name = "Pâtes de blé entier",
                 Category = "Pâtes",
                 CreationDate = DateTime.Now,
-                Price = 2.5M,
+                Price = 3.5M,
                 ExpirationDate = new DateTime(2025, 1, 30),
-                Description = "Pâtes 100% blé entier pour des repas sains",
+                Description = "Ingrédients : Semoule de blé dur entière, eau, tous les ingrédients sont issus de l'agriculture biologique",
                 Stock = 50,
                 SupplierId = 1,
-                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "miel_product.jpg"))
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "pates_blé_product.jpg"))
             });
 
             _DBContext.Products.Add(new Product()
@@ -89,12 +89,12 @@ namespace AgriNov.Models
                 Name = "Huile d'olive extra vierge",
                 Category = "Huiles",
                 CreationDate = DateTime.Now,
-                Price = 7.5M,
+                Price = 10.5M,
                 ExpirationDate = new DateTime(2026, 5, 15),
-                Description = "Huile d'olive de première pression à froid",
+                Description = "Huile d'olive de première pression à froid. Ingrédients : Olives, tous les ingrédients sont issus de l'agriculture biologique",
                 Stock = 20,
                 SupplierId= 1,
-                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "lait_product.jpg"))
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "huile_olive_product.jpg"))
             });
 
             _DBContext.Products.Add(new Product()
@@ -115,25 +115,12 @@ namespace AgriNov.Models
                 Name = "Coulis de tomates",
                 Category = "Conserves",
                 CreationDate = DateTime.Now,
-                Price = 3,
+                Price = 4,
                 ExpirationDate = new DateTime(2024, 11, 10),
                 Description = "Ingrédients : Tomates issues de l'Agriculture Biologique (99,6%), sel de Guérande, tous les ingrédients sont issus de l'agriculture biologique",
                 Stock = 15,
                 SupplierId = 2,
                 ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "coulis_tomates_product.jpg"))
-            });
-
-            _DBContext.Products.Add(new Product()
-            {
-                Name = "Thé vert Matcha",
-                Category = "Thés",
-                CreationDate = DateTime.Now,
-                Price = 9,
-                ExpirationDate = new DateTime(2025, 4, 18),
-                Description = "Thé vert matcha en poudre",
-                Stock = 8,
-                SupplierId = 4,
-                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "miel_product.jpg"))
             });
 
             _DBContext.Products.Add(new Product()
@@ -154,12 +141,25 @@ namespace AgriNov.Models
                 Name = "Jus d'orange",
                 Category = "Fruits",
                 CreationDate = DateTime.Now,
-                Price = 4.5M,
+                Price = 3.5M,
                 ExpirationDate = new DateTime(2026, 3, 15),
                 Description = "Ingrédients : Jus d'orange (96%), pulpe d'orange (4%), tous les ingrédients sont issus de l'agriculture biologique",
                 Stock = 40,
                 SupplierId = 2,
                 ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "jus_orange_product.jpg"))
+            });
+            
+            _DBContext.Products.Add(new Product()
+            {
+                Name = "Jus de pommes",
+                Category = "Fruits",
+                CreationDate = DateTime.Now,
+                Price = 4,
+                ExpirationDate = new DateTime(2026, 3, 15),
+                Description = "Ingrédients : Pommes, eau, tous les ingrédients sont issus de l'agriculture biologique",
+                Stock = 40,
+                SupplierId = 2,
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "jus_pommes_product.jpg"))
             });
 
             _DBContext.Products.Add(new Product()
@@ -167,12 +167,90 @@ namespace AgriNov.Models
                 Name = "Confiture de fraises",
                 Category = "Confitures",
                 CreationDate = DateTime.Now,
-                Price = 3.5M,
+                Price = 4.5M,
                 ExpirationDate = new DateTime(2025, 6, 20),
-                Description = "Confiture artisanale de fraises",
+                Description = "Ingrédients : Fraises, sucre de canne, tous les ingrédients sont issus de l'agriculture biologique",
                 Stock = 12,
                 SupplierId = 3,
-                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "confiture_myrtilles_product.jpg"))
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "confiture_fraises_product.jpg"))
+            });
+            
+            _DBContext.Products.Add(new Product()
+            {
+                Name = "Bière blonde",
+                Category = "Boisson",
+                CreationDate = DateTime.Now,
+                Price = 6.5M,
+                ExpirationDate = new DateTime(2025, 6, 20),
+                Description = "Lot de 3. Ingrédients : Eau, malt d'orge, malt de blé, sucre de canne, houblon, levure, tous les ingrédients sont issus de l'agriculture biologique",
+                Stock = 12,
+                SupplierId = 5,
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "pack_biere_blonde_product.jpg"))
+            });
+            
+            _DBContext.Products.Add(new Product()
+            {
+                Name = "Bière IPA",
+                Category = "Boisson",
+                CreationDate = DateTime.Now,
+                Price = 8M,
+                ExpirationDate = new DateTime(2025, 6, 20),
+                Description = "Lot de 3. Ingrédients : Eau, malt d'orge, malt de blé, sucre de canne, houblon, levure, tous les ingrédients sont issus de l'agriculture biologique",
+                Stock = 12,
+                SupplierId = 5,
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "pack_biere_ipa_product.jpg"))
+            });
+            
+            _DBContext.Products.Add(new Product()
+            {
+                Name = "Bière brune",
+                Category = "Boisson",
+                CreationDate = DateTime.Now,
+                Price = 7.5M,
+                ExpirationDate = new DateTime(2025, 6, 20),
+                Description = "Lot de 3. Ingrédients : Eau, malt d'orge, malt de blé, sucre de canne, houblon, levure, tous les ingrédients sont issus de l'agriculture biologique",
+                Stock = 12,
+                SupplierId = 5,
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "pack_biere_brune_product.jpg"))
+            });
+            
+            _DBContext.Products.Add(new Product()
+            {
+                Name = "Vin Rouge 2022",
+                Category = "Boisson",
+                CreationDate = DateTime.Now,
+                Price = 6.5M,
+                ExpirationDate = new DateTime(2025, 6, 20),
+                Description = "Ingrédients : raisins  issus du cépage Melon de Bourgogne, tous les ingrédients sont issus de l'agriculture biologique",
+                Stock = 12,
+                SupplierId = 2,
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "vin_rouge_product.jpg"))
+            });
+            
+            _DBContext.Products.Add(new Product()
+            {
+                Name = "Vin Blanc 2022",
+                Category = "Boisson",
+                CreationDate = DateTime.Now,
+                Price = 7.5M,
+                ExpirationDate = new DateTime(2025, 6, 20),
+                Description = "Ingrédients : raisins  issus du cépage Melon de Bourgogne, tous les ingrédients sont issus de l'agriculture biologique",
+                Stock = 12,
+                SupplierId = 2,
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "vin_blanc_product.jpg"))
+            });
+            
+            _DBContext.Products.Add(new Product()
+            {
+                Name = "Cuvée Chateau-Thébaud 2022",
+                Category = "Boisson",
+                CreationDate = DateTime.Now,
+                Price = 18.5M,
+                ExpirationDate = new DateTime(2025, 6, 20),
+                Description = "Lot de 3, vin rouge, rosé et blanc. Ingrédients : raisins  issus du cépage Melon de Bourgogne, tous les ingrédients sont issus de l'agriculture biologique",
+                Stock = 12,
+                SupplierId = 2,
+                ImgProduct = File.ReadAllBytes(Path.Combine(dirPath, "lot_vins_product.jpg"))
             });
 
             _DBContext.Products.Add(new Product()
